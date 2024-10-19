c77f2b76f602b2ab73db4b5c5687e35971a4eca2
Kirill <razdor.2019@mail.ru>
2024-10-19 21:23:03 +0300

    feat(main.js & subscribe-block) create success-message after sending an email

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
-const email = document.getElementById('email')
+const subscribeEmail = document.getElementById('subscribe-email')
+const subscribeCheckbox = document.getElementById('subscribe-checkbox')
+const subscribeForm = document.getElementById('subscribe-form')
+const successText = document.querySelector('.subscribe-block__success')
 
-email.addEventListener('input', function (event) {
+subscribeEmail.addEventListener('input', function (event) {
 	if (this.validity.typeMismatch) {
 		this.setCustomValidity('Formato de email inválido, verifique a ortografia')
 	} else {
 		this.setCustomValidity('')
+	}
+})
+subscribeForm.addEventListener('submit', event => {
+	event.preventDefault()
+
+	if (subscribeEmail.checkValidity() && subscribeCheckbox.checked) {
+		successText.classList.remove('hidden')
+		subscribeForm.classList.add('hidden')
+	} else {
+		if (!subscribeEmail.checkValidity()) {
+			subscribeEmail.reportValidity()
+		}
 	}
 })
