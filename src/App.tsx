2f45c1aff088a0676e02fee8f79507876433ebee
Kirill <razdor.2019@mail.ru>
2024-11-06 23:49:39 +0300

    feat: запрос к данным меню, лоадер и хедер

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from './containers/Header/Header.tsx'
+import fetchMenu from './services/fetchMenu.ts'
+import { Menu } from './interfaces/MenuInterface.ts'
+import K3Loader from './components/loaders/K3Loader.tsx'
 
-const App = function () {
-  return <div>Hello DS</div>
+function App() {
+  const [menuData, setMenuData] = useState<Menu | undefined>(undefined)
+  const [isLoading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchMenu()
+      console.log(data)
+      setMenuData(data)
+      setLoading(false)
+    }
+    setTimeout(() => fetchData(), 1500)
+  }, [])
+
+  return (
+    <>
+      {isLoading && <K3Loader />}
+      {menuData && <Header content={menuData} />}
+    </>
+  )
 }
 
 export default App
