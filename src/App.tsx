793267c667c50921d12ddd60894942a15a674e92
Kirill <razdor.2019@mail.ru>
2024-11-07 21:36:18 +0300

    feat: добавить в App компонент-контейнер ArticlePreview который рендерит компонент Article

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,28 @@ import K3Loader from './components/loaders/k3Loader.tsx'
 import { default as requestToData } from './services/fetchData.ts'
 import useData from './hooks/useData.ts'
 import { useTheme } from './components/ThemeContext.tsx'
-
+import ArticlePreview from './containers/ArticlePreview/ArticlePreview.tsx'
+import { Article } from './components/Article.tsx'
 function App() {
   const { theme, toggleTheme } = useTheme()
   const { contentData, isLoading, error } = useData(requestToData)
   useEffect(() => {
     toggleTheme()
   }, [])
+
   return (
     <>
       {isLoading && <K3Loader />}
       {error && <p>{error}</p>}
       {contentData && <Header content={contentData.menu} />}
+
+      {contentData && (
+        <main>
+          <ArticlePreview>
+            <Article content={contentData.sections.main.items[0]} />
+          </ArticlePreview>
+        </main>
+      )}
     </>
   )
 }
