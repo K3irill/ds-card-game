3d650a355bc4446572abfe90db15a9359739469e
Kirill <razdor.2019@mail.ru>
2024-11-08 22:25:31 +0300

    feat: вставить компонент RunningLine в ArticlePreview и подправить padding-и

--- src/containers/ArticlePreview/ArticlePreview.tsx
+++ src/containers/ArticlePreview/ArticlePreview.tsx
@@ -1,16 +1,21 @@
 import React, { Children, ReactNode, useEffect } from 'react'
 import styles from './ArticlePreview.module.scss'
-
+import RunningLine from '../../components/running-line/RunningLine'
+import { Ticker } from '../../interfaces/DataInterface'
 interface ArticlePreviewProps {
   children: ReactNode
+  ticker: Ticker
 }
-function ArticlePreview({ children }: ArticlePreviewProps) {
+function ArticlePreview({ children, ticker }: ArticlePreviewProps) {
   return (
-    <section className={styles['article-preview']}>
-      <div className={`${styles['article-preview__container']} __container`}>
-        {children}
-      </div>
-    </section>
+    <>
+      <section className={styles['article-preview']}>
+        <div className={`${styles['article-preview__container']} __container`}>
+          {children}
+        </div>
+      </section>
+      <RunningLine ticker={ticker} mod={'articles'} />
+    </>
   )
 }
 
