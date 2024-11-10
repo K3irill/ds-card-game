228514be213fb434d4f9effc501148495cfcd2cf
Kirill <razdor.2019@mail.ru>
2024-11-10 22:08:36 +0300

    feat: добавить в функцию форматирование двух дат

--- src/services/formatSpainDate.ts
+++ src/services/formatSpainDate.ts
@@ -1,14 +1,30 @@
-const formatSpainDate = (dateStr: string) => {
-  const [day, month, year] = dateStr.split('.')
-
-  const date = new Date(`${year}-${month}-${day}`)
+const formatSpainDate = (dateFromStr: string, dateToStr?: string) => {
+  const [dayFrom, monthFrom, yearFrom] = dateFromStr.split('.')
+  const dateFrom = new Date(`${yearFrom}-${monthFrom}-${dayFrom}`)
 
   const options: Intl.DateTimeFormatOptions = {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   }
-  const spainDate = new Intl.DateTimeFormat('es-ES', options).format(date)
-  return spainDate
+
+  const formattedFrom = new Intl.DateTimeFormat('es-ES', options).format(
+    dateFrom,
+  )
+
+  if (dateToStr) {
+    const [dayTo, monthTo, yearTo] = dateToStr.split('.')
+    const dateTo = new Date(`${yearTo}-${monthTo}-${dayTo}`)
+    const formattedTo = new Intl.DateTimeFormat('es-ES', options).format(dateTo)
+
+    if (dateFrom.getFullYear() === dateTo.getFullYear()) {
+      return `${dayFrom} de ${formattedFrom.split(' ')[2]},  ${
+        parseInt(dayTo) < 10 ? '0' + (parseInt(dayTo) - 1) : parseInt(dayTo) - 1
+      } e ${dayTo} de ${formattedTo.split(' ')[2]} de ${yearTo}`
+    }
+    return `${formattedFrom}, ${formattedTo}`
+  }
+  return formattedFrom
 }
+
 export default formatSpainDate
