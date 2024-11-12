f992b330e91f76344db10ea42a40517a6ec9cf31
Kirill <razdor.2019@mail.ru>
2024-11-12 18:25:46 +0300

    feat: создать компонент-контейнер SubscribeBlock и стили

---
+++ src/containers/SubscribeBlock/SubscribeBlock.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode, useEffect } from 'react'
+import styles from './SubscribeBlock.module.scss'
+import {
+  Subscription,
+  SubscriptionSection,
+} from '../../interfaces/DataInterface'
+
+import SubscribeForm from '../../components/forms/SubscribeForm'
+interface SubscribeBlockProps {
+  content: SubscriptionSection
+}
+function SubscribeBlock({ content }: SubscribeBlockProps) {
+  return (
+    <>
+      <section className={styles['subscribe-block']}>
+        <div className={`${styles['subscribe-block__container']} __container`}>
+          <div className={`${styles['subscribe-block__content-wrapper']} `}>
+            <div className={`${styles['subscribe-block__content']}`}>
+              <div className={`${styles['subscribe-block__description']}`}>
+                <h2>{content.title}</h2>
+                <p>{content.text}</p>
+              </div>
+              <div className={`${styles['subscribe-block__form']}`}>
+                <SubscribeForm content={content} />
+              </div>
+            </div>
+          </div>
+          <div className={`${styles['subscribe-block__illustration']}`}></div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default SubscribeBlock
