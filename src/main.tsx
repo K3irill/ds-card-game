f533484969baacbc7434ed4759c1bdedcd3637df
Kirill <razdor.2019@mail.ru>
2024-11-12 18:21:26 +0300

    chore: изменить пути к файлам

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './assets/styles/global.scss'
-import { ThemeProvider } from './components/ThemeContext.tsx'
+import { ThemeProvider } from './components/theme-provider/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
