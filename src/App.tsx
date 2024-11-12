139242ea94e4fd9ccf9fb6af0eb4bfff8fa90b47
Kirill <razdor.2019@mail.ru>
2024-11-12 18:30:12 +0300

    feat:добавить SubscribeBlock в App и изменить путь к ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import K3Loader from './components/loaders/k3Loader.tsx'
 // eslint-disable-next-line import/no-named-default
 import { default as requestToData } from './services/fetchData.ts'
 import useData from './hooks/useData.ts'
-import { useTheme } from './components/ThemeContext.tsx'
+import { useTheme } from './components/theme-provider/ThemeContext.tsx'
 import ArticlePreview from './containers/ArticlePreview/ArticlePreview.tsx'
 import { Article } from './components/Article.tsx'
 import ArticlesBlock from './containers/ArticleBlock/ArticlesBlock.tsx'
 import WebinarsBlock from './containers/WebinarsBlock/WebinarsBlock.tsx'
-
+import SubscribeBlock from './containers/SubscribeBlock/SubscribeBlock.tsx'
 function App() {
   const { theme, toggleTheme } = useTheme()
   const { contentData, isLoading, error } = useData(requestToData)
@@ -30,13 +30,14 @@ function App() {
               <Article content={contentData.sections.main.items[0]} />
             </ArticlePreview>
             <ArticlesBlock content={contentData.sections.content}>
-              {contentData.sections.content.items.map((item) => {
-                return <Article content={item} />
+              {contentData.sections.content.items.map((item, index) => {
+                return <Article key={index} content={item} />
               })}
             </ArticlesBlock>
             <WebinarsBlock
               content={contentData.sections.proposals}
             ></WebinarsBlock>
+            <SubscribeBlock content={contentData.sections.subscription} />
           </main>
         </>
       )}
