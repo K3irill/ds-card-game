f42c58f012dcd477e8a746f5f0623c5f6744761c
Kirill <razdor.2019@mail.ru>
2024-11-14 18:40:59 +0300

    feat: использовать хук useHideHeader

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
 import NavButton from '../../components/buttons/NavButton.tsx'
 import { Menu } from '../../interfaces/DataInterface.ts'
 import IconButton from '../../components/buttons/IconButton.tsx'
 import Logo from '../../components/forImg/Logo.tsx'
+import useHideHeader from '../../hooks/useHideHeader.ts'
 
 interface HeaderProps {
   content: Menu
 }
 function Header({ content }: HeaderProps) {
+  const [headerHidden, setHeaderHidden] = useHideHeader()
+
   return (
-    <header className={`${styles.header} }`}>
+    <header
+      className={`${styles.header} ${styles['header--' + headerHidden]} }`}
+    >
       <div className={`${styles.header__container} __container`}>
         <div className={styles['header__logo-block']}>
           <Logo url={content.logo} />
