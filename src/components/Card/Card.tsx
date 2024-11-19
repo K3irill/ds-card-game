184fb91592d374ccb9f3d42ea8ea2d796f9f82af
Kirill <razdor.2019@mail.ru>
2024-11-19 19:47:54 +0300

    feat: создан компонент Card с типизацией объекта карточки

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './Card.module.scss'
+import { CardItem } from '../../interfaces/card.interface'
+
+interface CardProps {
+  cardObject: CardItem
+  handleCardClick: any // ПОТОМ ИСПРАВИТЬ!!!!
+}
+
+const Card = ({ cardObject, handleCardClick }: CardProps) => {
+  const [card, setCard] = useState(cardObject)
+
+  useEffect(() => {
+    setCard(cardObject)
+  }, [cardObject])
+
+  return (
+    <div
+      className={`${styles.card} ${
+        card.isInverted ? styles.card__inverted : ''
+      }`}
+      onClick={() => handleCardClick(card)}
+    >
+      <div className={styles.card__inside}>
+        <div className={styles.card__front}>
+          <div className={styles.card__front_background}></div>
+        </div>
+        <div className={styles.card__back}>
+          <div className={styles.card__back_image}>
+            <img
+              className={styles.card__image}
+              src={card.img}
+              alt="Card Image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
