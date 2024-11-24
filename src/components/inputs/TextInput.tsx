8e2bbe738c0a092e808582c5df485343c6d0ab2b
Kirill <razdor.2019@mail.ru>
2024-11-24 15:07:52 +0300

    feat: пришлось применить дженерик

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import styles from './TextInput.module.scss'
 
-interface TextInputProps {
-  value: number | string
-  funOnChange: () => void
+interface TextInputProps<T extends string | number> {
+  value: T
+  funOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   placeholder: string
-  setValue: React.Dispatch<React.SetStateAction<number | string>>
+  setValue: React.Dispatch<React.SetStateAction<T>>
   type: 'number' | 'text' | undefined
 }
 
-const TextInput = ({
+const TextInput = <T extends string | number>({
   value,
   funOnChange,
   placeholder,
   setValue,
   type,
-}: TextInputProps) => {
+}: TextInputProps<T>) => {
   return (
     <div className={styles['text-input__wrapper']}>
       <input
@@ -26,7 +26,7 @@ const TextInput = ({
         placeholder={placeholder}
       />
       <button
-        onClick={() => setValue('')}
+        onClick={() => setValue('' as T)}
         className={styles['text-input__clear-btn']}
       >
         X
